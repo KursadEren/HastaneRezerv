c3cee038cb00ae52164eb827ca63707cf8cced4d
KursadEren <k.erenmaden1@outlook.com>
2023-12-01 00:59:52 +0300

    Login Page

---
+++ HastaneRezerv/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System;
+
+namespace HastaneRezerv.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpPost]
+        public IActionResult LoginOlustur()
+        {
+
+            return RedirectToAction("Hastane");
+        }
+        public IActionResult Hastane()
+        {
+            // Hastane sayfasının işlemleri
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+      
+    }
+}
