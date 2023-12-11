d4e655e2cffa423c5a9909c488b5a595c781006a
KursadEren <k.erenmaden1@outlook.com>
2023-12-11 16:11:20 +0300

    check

--- HastaneRezerv/Controllers/HomeController.cs
+++ HastaneRezerv/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace HastaneRezerv.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
